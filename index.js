499682ccf989fecb9a23773aa701bd06391e1aa5
otisnth <otisnth@gmail.com>
2024-03-27 18:33:29 +0300

    feat: доработки бота

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import amqplib from "amqplib";
+import fs from "fs";
 import dotenv from "dotenv";
-import { Telegraf, Scenes, session, Markup } from "telegraf";
+import { Telegraf, Scenes, session, Markup, Input } from "telegraf";
 import { Client } from "basic-ftp";
 
 dotenv.config();
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 const rabbit = amqplib.connect(process.env.RABBIT_URL);
 
+// Определения клавиатур
 const countryKeyboard = Markup.inlineKeyboard([
     [
         Markup.button.callback("🇨🇳", JSON.stringify({ title: "Китайская", country: "Chinese" }), false),
@@ -65,88 +67,204 @@ const getReceptKeyboard = Markup.keyboard(["Получить рецепты"]).r
 const removeKeyboard = Markup.removeKeyboard();
 const toMainKeyboard = Markup.keyboard(["На главную"]).resize();
 
-const receptForm = new Scenes.WizardScene(
-    "receptForm",
+// Функции для работы с FTP
 
-    async (ctx) => {
-        await ctx.reply("Выбранные параметры: ", toMainKeyboard);
-        await ctx.reply("Выберите желаемую кухню: ", countryKeyboard);
-        ctx.wizard.state.data = {};
-        return ctx.wizard.next();
-    },
-    async (ctx) => {
-        if (!ctx.callbackQuery) {
-            if (ctx.message.text === "На главную") return ctx.scene.leave();
-            return;
-        }
-        await ctx.deleteMessage();
-        const callbackData = JSON.parse(ctx.callbackQuery.data);
-        await ctx.reply(`${callbackData.title} кухня`);
-        ctx.wizard.state.data.country = callbackData.country;
-        await ctx.reply("Выберите тип блюда: ", mealTypesKeyboard);
-        return ctx.wizard.next();
-    },
-    async (ctx) => {
-        if (!ctx.callbackQuery) {
-            if (ctx.message.text === "На главную") return ctx.scene.leave();
-            return;
-        }
-        await ctx.deleteMessage();
-
-        const callbackData = JSON.parse(ctx.callbackQuery.data);
-        await ctx.reply(`${callbackData.title} тип блюда`);
-        ctx.wizard.state.data.type = callbackData.mealType;
-        await ctx.reply("Введите желаемое количество рецептов (1-10): ");
-        return ctx.wizard.next();
-    },
-    async (ctx) => {
-        if (ctx.message.text === "На главную") return ctx.scene.leave();
-        if (isNaN(ctx.message.text)) {
-            await ctx.reply("Введите корректное число");
-            await ctx.deleteMessage();
-
-            return;
-        }
-        if (ctx.message.text > 10 || ctx.message.text < 1) {
-            await ctx.reply("Введите число в диапазоне 1-10");
-            await ctx.deleteMessage();
-
-            return;
-        }
-        await ctx.deleteMessage();
-        ctx.wizard.state.data.count = ctx.message.text;
-        ctx.wizard.state.data.telegram_id = ctx.update.message.chat.id;
-
-        await ctx.reply(`Количество рецептов: ${ctx.message.text}`);
-        console.log(ctx.wizard.state.data);
-        return ctx.scene.leave();
+async function downloadFromFTP(localPath, remotePath) {
+    const client = new Client();
+    client.ftp.verbose = true;
+    try {
+        await client.access({
+            host: process.env.FTP_URL,
+            user: process.env.FTP_LOGIN,
+            password: process.env.FTP_PASS,
+        });
+        await client.downloadTo(localPath, remotePath);
+    } catch (err) {
+        console.log(err);
     }
-);
+    client.close();
+}
 
-receptForm.leave(async (ctx) => {
-    await ctx.reply("Добро пожаловать в ВВС!", getReceptKeyboard);
-});
+async function removeFromFTP(remotePath) {
+    const client = new Client();
+    client.ftp.verbose = true;
+    try {
+        await client.access({
+            host: process.env.FTP_URL,
+            user: process.env.FTP_LOGIN,
+            password: process.env.FTP_PASS,
+        });
+        await client.remove(remotePath);
+    } catch (err) {
+        console.log(err);
+    }
+    client.close();
+}
 
-const stage = new Scenes.Stage([receptForm]);
+// Работа кролика
+rabbit
+    .then(function (connection) {
+        var ok = connection.createChannel();
+        ok.then(function (channel) {
+            // Конфигурация кролика
+            channel.assertQueue("receptRequest");
+            channel.assertQueue("receptResponse");
+            channel.assertExchange("recept");
+            channel.bindQueue("receptRequest", "recept", "rreq");
+            channel.bindQueue("receptResponse", "recept", "rres");
 
-bot.use(session());
-bot.use(stage.middleware());
+            let msgUpd;
+            // Сцена формы запроса рецептов
+            const receptForm = new Scenes.WizardScene(
+                "receptForm",
+                async (ctx) => {
+                    msgUpd = await ctx.reply("⚙️Выбранные параметры: ", removeKeyboard);
+                    await ctx.reply("Выберите желаемую кухню: ", countryKeyboard);
+                    ctx.wizard.state.data = {};
+                    return ctx.wizard.next();
+                },
+                async (ctx) => {
+                    if (!ctx.callbackQuery) {
+                        if (ctx.message.text === "/start") return ctx.scene.leave();
+                        await ctx.deleteMessage();
 
-bot.telegram.setMyCommands([
-    { command: "start", description: "На главную" },
-    { command: "help", description: "Помощь" },
-]);
+                        return;
+                    }
 
-bot.start(async (ctx) => {
-    await ctx.reply("Добро пожаловать в ВВС!", getReceptKeyboard);
-});
+                    const callbackData = JSON.parse(ctx.callbackQuery.data);
+                    ctx.wizard.state.messageText = `🌍${callbackData.title} кухня \n`;
+                    await ctx.editMessageText(ctx.wizard.state.messageText + "Выберите тип блюда: ", mealTypesKeyboard);
 
-bot.help(async (ctx) => {
-    await ctx.reply("Для начала работы введите /start");
-});
+                    ctx.wizard.state.data.country = callbackData.country;
 
-bot.hears("Получить рецепты", async (ctx) => await ctx.scene.enter("receptForm"));
+                    return ctx.wizard.next();
+                },
+                async (ctx) => {
+                    if (!ctx.callbackQuery) {
+                        if (ctx.message.text === "/start") return ctx.scene.leave();
+                        await ctx.deleteMessage();
 
-bot.hears("На главную", async (ctx) => await ctx.reply("Добро пожаловать в ВВС!", getReceptKeyboard));
+                        return;
+                    }
 
-bot.launch();
+                    const callbackData = JSON.parse(ctx.callbackQuery.data);
+                    ctx.wizard.state.messageText += `🍽️Тип блюда: ${callbackData.title}\n`;
+                    msgUpd = await ctx.editMessageText(
+                        ctx.wizard.state.messageText + "Введите желаемое количество рецептов (1-10): "
+                    );
+
+                    ctx.wizard.state.data.type = callbackData.mealType;
+
+                    return ctx.wizard.next();
+                },
+                async (ctx) => {
+                    if (ctx.message.text === "/start") return ctx.scene.leave();
+                    if (isNaN(ctx.message.text)) {
+                        await ctx.telegram.editMessageText(
+                            msgUpd.chat.id,
+                            msgUpd.message_id,
+                            undefined,
+                            ctx.wizard.state.messageText +
+                                "Введите желаемое количество рецептов (1-10): \n❗Введите число"
+                        );
+                        await ctx.deleteMessage();
+
+                        return;
+                    }
+                    if (ctx.message.text > 10 || ctx.message.text < 1) {
+                        await ctx.telegram.editMessageText(
+                            msgUpd.chat.id,
+                            msgUpd.message_id,
+                            undefined,
+                            ctx.wizard.state.messageText +
+                                "Введите желаемое количество рецептов (1-10): \n❗Введите число в диапазоне 1-10"
+                        );
+                        await ctx.deleteMessage();
+
+                        return;
+                    }
+                    await ctx.deleteMessage();
+                    ctx.wizard.state.data.count = ctx.message.text;
+                    ctx.wizard.state.data.telegram_id = ctx.update.message.chat.id;
+
+                    ctx.wizard.state.messageText += `📋Количество рецептов: ${ctx.message.text}\n`;
+
+                    await ctx.telegram.editMessageText(
+                        msgUpd.chat.id,
+                        msgUpd.message_id,
+                        undefined,
+                        ctx.wizard.state.messageText
+                    );
+
+                    channel.publish("recept", "rreq", Buffer.from(JSON.stringify(ctx.wizard.state.data)));
+                    ctx.wizard.state.messageText += `✅Запрос успешно отправлен! Ожидайте`;
+                    await ctx.telegram.editMessageText(
+                        msgUpd.chat.id,
+                        msgUpd.message_id,
+                        undefined,
+                        ctx.wizard.state.messageText
+                    );
+
+                    return ctx.scene.leave();
+                }
+            );
+
+            receptForm.leave(async (ctx) => {
+                await ctx.reply("Выберите следующее действие", getReceptKeyboard);
+            });
+
+            // Конфигурация бота
+            const stage = new Scenes.Stage([receptForm]);
+
+            bot.use(session());
+            bot.use(stage.middleware());
+
+            bot.telegram.setMyCommands([
+                { command: "start", description: "На главную" },
+                { command: "help", description: "Помощь" },
+            ]);
+
+            bot.start(async (ctx) => {
+                await ctx.reply("Добро пожаловать в ВВС!", getReceptKeyboard);
+            });
+
+            bot.help(async (ctx) => {
+                await ctx.reply("Для начала работы введите /start");
+            });
+
+            bot.hears("Получить рецепты", async (ctx) => await ctx.scene.enter("receptForm"));
+
+            bot.hears("На главную", async (ctx) => await ctx.reply("Добро пожаловать в ВВС!", getReceptKeyboard));
+
+            bot.launch();
+
+            channel.consume("receptResponse", async function (message) {
+                let data = JSON.parse(message.content.toString());
+                if (data.status === "error") {
+                    await bot.telegram.sendMessage(data.telegram_id, "❗" + data.message);
+                    channel.ack(message);
+                    return;
+                }
+                try {
+                    await downloadFromFTP(`./storage/${data.file.split("/").at(-1)}`, data.file);
+                    await bot.telegram.sendDocument(
+                        data.telegram_id,
+                        Input.fromLocalFile(`./storage/${data.file.split("/").at(-1)}`),
+                        { caption: "📕Ваша подборка рецептов!" }
+                    );
+                    fs.unlinkSync(`./storage/${data.file.split("/").at(-1)}`);
+                    await removeFromFTP(data.file);
+                } catch (err) {
+                    if (data?.telegram_id) {
+                        await bot.telegram.sendMessage(
+                            data.telegram_id,
+                            "❗Произошла ошибка при загрузке файла. Попробуйте еще раз"
+                        );
+                    }
+                }
+                channel.ack(message);
+            });
+        });
+        return ok;
+    })
+    .then(null, console.log);
