68b5d1440c9cfff253d6cb6995fc81f0c89b8f42
otisnth <otisnth@gmail.com>
2024-03-27 13:35:26 +0300

    feat: реализована форма выбора рецептов

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ const rabbit = amqplib.connect(process.env.RABBIT_URL);
 
 const countryKeyboard = Markup.inlineKeyboard([
     [
-        Markup.button.callback("🇨🇳", JSON.stringify({ title: "🇨🇳 Китайская", country: "Chinese" }), false),
-        Markup.button.callback("🇮🇱", JSON.stringify({ title: "🇮🇱 Израильская", country: "Jewish" }), false),
-        Markup.button.callback("🇩🇪", JSON.stringify({ title: "🇩🇪 Немецкая", country: "German" }), false),
-        Markup.button.callback("🇪🇸", JSON.stringify({ title: "🇪🇸 Испанская", country: "Spanish" }), false),
+        Markup.button.callback("🇨🇳", JSON.stringify({ title: "Китайская", country: "Chinese" }), false),
+        Markup.button.callback("🇮🇱", JSON.stringify({ title: "Израильская", country: "Jewish" }), false),
+        Markup.button.callback("🇩🇪", JSON.stringify({ title: "Немецкая", country: "German" }), false),
+        Markup.button.callback("🇪🇸", JSON.stringify({ title: "Испанская", country: "Spanish" }), false),
     ],
     [
-        Markup.button.callback("🇪🇺", JSON.stringify({ title: "🇪🇺 Европейская", country: "European" }), false),
-        Markup.button.callback("🇫🇷", JSON.stringify({ title: "🇫🇷 Французская", country: "French" }), false),
-        Markup.button.callback("🇬🇧", JSON.stringify({ title: "🇬🇧 Британская", country: "British" }), false),
-        Markup.button.callback("🇬🇷", JSON.stringify({ title: "🇬🇷 Греческая", country: "Greek" }), false),
+        Markup.button.callback("🇪🇺", JSON.stringify({ title: "Европейская", country: "European" }), false),
+        Markup.button.callback("🇫🇷", JSON.stringify({ title: "Французская", country: "French" }), false),
+        Markup.button.callback("🇬🇧", JSON.stringify({ title: "Британская", country: "British" }), false),
+        Markup.button.callback("🇬🇷", JSON.stringify({ title: "Греческая", country: "Greek" }), false),
     ],
     [
-        Markup.button.callback("🇮🇳", JSON.stringify({ title: "🇮🇳 Индийская", country: "Indian" }), false),
-        Markup.button.callback("🇮🇪", JSON.stringify({ title: "🇮🇪 Ирландская", country: "Irish" }), false),
-        Markup.button.callback("🇮🇹", JSON.stringify({ title: "🇮🇹 Итальянская", country: "Italian" }), false),
-        Markup.button.callback("🇯🇵", JSON.stringify({ title: "🇯🇵 Японская", country: "Japanese" }), false),
+        Markup.button.callback("🇮🇳", JSON.stringify({ title: "Индийская", country: "Indian" }), false),
+        Markup.button.callback("🇮🇪", JSON.stringify({ title: "Ирландская", country: "Irish" }), false),
+        Markup.button.callback("🇮🇹", JSON.stringify({ title: "Итальянская", country: "Italian" }), false),
+        Markup.button.callback("🇯🇵", JSON.stringify({ title: "Японская", country: "Japanese" }), false),
     ],
     [
-        Markup.button.callback("🇰🇷", JSON.stringify({ title: "🇰🇷 Корейская", country: "Korean" }), false),
-        Markup.button.callback("🇲🇽", JSON.stringify({ title: "🇲🇽 Мексиканская", country: "Mexican" }), false),
-        Markup.button.callback("🇹🇭", JSON.stringify({ title: "🇹🇭 Тайская", country: "Thai" }), false),
-        Markup.button.callback("🇺🇸", JSON.stringify({ title: "🇺🇸 Американская", country: "American" }), false),
+        Markup.button.callback("🇰🇷", JSON.stringify({ title: "Корейская", country: "Korean" }), false),
+        Markup.button.callback("🇲🇽", JSON.stringify({ title: "Мексиканская", country: "Mexican" }), false),
+        Markup.button.callback("🇹🇭", JSON.stringify({ title: "Тайская", country: "Thai" }), false),
+        Markup.button.callback("🇺🇸", JSON.stringify({ title: "Американская", country: "American" }), false),
     ],
 ]).resize();
 
@@ -38,101 +38,94 @@ const mealTypesKeyboard = Markup.inlineKeyboard([
     [
         Markup.button.callback(
             "Основное блюдо",
-            JSON.stringify({ a: "selectMealType", mealType: "main course" }),
+            JSON.stringify({ title: "Основное блюдо", mealType: "main course" }),
             false
         ),
-        Markup.button.callback("Гарнир", JSON.stringify({ a: "selectMealType", mealType: "side dish" }), false),
+        Markup.button.callback("Гарнир", JSON.stringify({ title: "Гарнир", mealType: "side dish" }), false),
     ],
     [
-        Markup.button.callback("Десерт", JSON.stringify({ a: "selectMealType", mealType: "dessert" }), false),
-        Markup.button.callback("Закуска", JSON.stringify({ a: "selectMealType", mealType: "appetizer" }), false),
+        Markup.button.callback("Десерт", JSON.stringify({ title: "Десерт", mealType: "dessert" }), false),
+        Markup.button.callback("Закуска", JSON.stringify({ title: "Закуска", mealType: "appetizer" }), false),
     ],
     [
-        Markup.button.callback("Салат", JSON.stringify({ a: "selectMealType", mealType: "salad" }), false),
-
-        Markup.button.callback("Напиток", JSON.stringify({ a: "selectMealType", mealType: "drink" }), false),
+        Markup.button.callback("Салат", JSON.stringify({ title: "Салат", mealType: "salad" }), false),
+        Markup.button.callback("Напиток", JSON.stringify({ title: "Напиток", mealType: "drink" }), false),
     ],
     [
-        Markup.button.callback("Завтрак", JSON.stringify({ a: "selectMealType", mealType: "breakfast" }), false),
-        Markup.button.callback("Суп", JSON.stringify({ a: "selectMealType", mealType: "soup" }), false),
+        Markup.button.callback("Завтрак", JSON.stringify({ title: "Завтрак", mealType: "breakfast" }), false),
+        Markup.button.callback("Суп", JSON.stringify({ title: "Суп", mealType: "soup" }), false),
     ],
     [
-        Markup.button.callback("Соус", JSON.stringify({ a: "selectMealType", mealType: "sauce" }), false),
-        Markup.button.callback("Выпечка", JSON.stringify({ a: "selectMealType", mealType: "bread" }), false),
+        Markup.button.callback("Соус", JSON.stringify({ title: "Соус", mealType: "sauce" }), false),
+        Markup.button.callback("Выпечка", JSON.stringify({ title: "Выпечка", mealType: "bread" }), false),
     ],
 ]);
 
+const getReceptKeyboard = Markup.keyboard(["Получить рецепты"]).resize();
+const removeKeyboard = Markup.removeKeyboard();
+const toMainKeyboard = Markup.keyboard(["На главную"]).resize();
+
 const receptForm = new Scenes.WizardScene(
     "receptForm",
+
     async (ctx) => {
+        await ctx.reply("Выбранные параметры: ", toMainKeyboard);
         await ctx.reply("Выберите желаемую кухню: ", countryKeyboard);
         ctx.wizard.state.data = {};
         return ctx.wizard.next();
     },
     async (ctx) => {
-        console.log(ctx.update.callback_query.message);
-        // await ctx.deleteMessage();
         if (!ctx.callbackQuery) {
+            if (ctx.message.text === "На главную") return ctx.scene.leave();
             return;
         }
+        await ctx.deleteMessage();
         const callbackData = JSON.parse(ctx.callbackQuery.data);
-        // console.log(ctx.callbackQuery.data);
-        // await ctx.reply(ctx.i18n.t("scenes.report.form.selectedInjury", { name: typeInjury.dataValues.name }));
-        // ctx.wizard.state.data.injury = typeInjury.dataValues.id;
-        // await ctx.reply(ctx.i18n.t("scenes.report.form.getDescription"), getSkipDescriptionKeyboard(ctx));
+        await ctx.reply(`${callbackData.title} кухня`);
+        ctx.wizard.state.data.country = callbackData.country;
+        await ctx.reply("Выберите тип блюда: ", mealTypesKeyboard);
         return ctx.wizard.next();
     },
-    // async (ctx) => {
-    //     if (!/^\d{4}\-\d{2}\-\d{2}$/.test(ctx.message.text)) {
-    //         await ctx.reply(ctx.i18n.t("scenes.report.errors.formatDate"));
-    //         return;
-    //     }
-    //     if (!moment(ctx.message.text, "YYYY-MM-DD").isValid()) {
-    //         await ctx.reply(ctx.i18n.t("scenes.report.errors.invalidDate"));
-    //         return;
-    //     }
-    //     ctx.wizard.state.data.date = ctx.message.text;
-    //     await ctx.reply(ctx.i18n.t("scenes.report.form.getInjury"), typeInjuryKeyboard);
-    //     return ctx.wizard.next();
-    // },
-    // async (ctx) => {
-    //     await ctx.deleteMessage();
-    //     if (!ctx.callbackQuery) {
-    //         return;
-    //     }
-    //     const callbackData = JSON.parse(ctx.callbackQuery.data);
-    //     const typeInjury = await TypeInjury.findByPk(callbackData.id);
-    //     await ctx.reply(ctx.i18n.t("scenes.report.form.selectedInjury", { name: typeInjury.dataValues.name }));
-    //     ctx.wizard.state.data.injury = typeInjury.dataValues.id;
-    //     await ctx.reply(ctx.i18n.t("scenes.report.form.getDescription"), getSkipDescriptionKeyboard(ctx));
-    //     return ctx.wizard.next();
-    // },
     async (ctx) => {
-        // if (ctx.callbackQuery) {
-        //     await ctx.deleteMessage();
-        //     ctx.wizard.state.data.description = "";
-        // } else {
-        //     ctx.wizard.state.data.description = ctx.message.text;
-        // }
-        // let incident = Incident.build({
-        //     fullName: ctx.wizard.state.data.fullName,
-        //     date: ctx.wizard.state.data.date,
-        //     injury: ctx.wizard.state.data.injury,
-        //     description: ctx.wizard.state.data.description,
-        // });
-        // if (ctx.session["reportLocation"].type === "room") {
-        //     incident.room = ctx.session["reportLocation"].id;
-        // } else {
-        //     incident.order = ctx.session["reportLocation"].id;
-        // }
-        // await incident.save();
+        if (!ctx.callbackQuery) {
+            if (ctx.message.text === "На главную") return ctx.scene.leave();
+            return;
+        }
+        await ctx.deleteMessage();
+
+        const callbackData = JSON.parse(ctx.callbackQuery.data);
+        await ctx.reply(`${callbackData.title} тип блюда`);
+        ctx.wizard.state.data.type = callbackData.mealType;
+        await ctx.reply("Введите желаемое количество рецептов (1-10): ");
+        return ctx.wizard.next();
+    },
+    async (ctx) => {
+        if (ctx.message.text === "На главную") return ctx.scene.leave();
+        if (isNaN(ctx.message.text)) {
+            await ctx.reply("Введите корректное число");
+            await ctx.deleteMessage();
+
+            return;
+        }
+        if (ctx.message.text > 10 || ctx.message.text < 1) {
+            await ctx.reply("Введите число в диапазоне 1-10");
+            await ctx.deleteMessage();
+
+            return;
+        }
+        await ctx.deleteMessage();
+        ctx.wizard.state.data.count = ctx.message.text;
+        ctx.wizard.state.data.telegram_id = ctx.update.message.chat.id;
+
+        await ctx.reply(`Количество рецептов: ${ctx.message.text}`);
+        console.log(ctx.wizard.state.data);
         return ctx.scene.leave();
     }
 );
 
-// receptForm.leave(async (ctx) => {
-//     await ctx.reply(ctx.i18n.t("scenes.start.nextStep"), getMainKeyboard(ctx));
-// });
+receptForm.leave(async (ctx) => {
+    await ctx.reply("Добро пожаловать в ВВС!", getReceptKeyboard);
+});
 
 const stage = new Scenes.Stage([receptForm]);
 
@@ -145,43 +138,15 @@ bot.telegram.setMyCommands([
 ]);
 
 bot.start(async (ctx) => {
-    await ctx.reply("Добро пожаловать в ВВС!");
-    await ctx.scene.enter("receptForm");
+    await ctx.reply("Добро пожаловать в ВВС!", getReceptKeyboard);
 });
 
 bot.help(async (ctx) => {
     await ctx.reply("Для начала работы введите /start");
 });
 
+bot.hears("Получить рецепты", async (ctx) => await ctx.scene.enter("receptForm"));
+
+bot.hears("На главную", async (ctx) => await ctx.reply("Добро пожаловать в ВВС!", getReceptKeyboard));
+
 bot.launch();
-
-// rabbit
-//     .then(function (connection) {
-//         var ok = connection.createChannel();
-//         ok.then(function (channel) {
-//             // durable: true is set by default
-//             channel.assertQueue("messages");
-//             channel.assertExchange("incoming");
-//             channel.bindQueue("messages", "incoming", "mda");
-//             channel.consume("messages", function (message) {
-//                 console.log(message.content.toString());
-//                 channel.ack(message);
-//             });
-//         });
-//         return ok;
-//     })
-//     .then(null, console.log);
-
-// rabbit
-//     .then(function (connection) {
-//         var ok = connection.createChannel();
-//         ok.then(function (channel) {
-//             // durable: true is set by default
-//             channel.assertQueue("messages");
-//             channel.assertExchange("incoming");
-//             channel.bindQueue("messages", "incoming", "mda");
-//             for (let i = 0; i < 1000; i++) channel.sendToQueue("messages", Buffer.from("Hello "));
-//         });
-//         return ok;
-//     })
-//     .then(null, console.log);
